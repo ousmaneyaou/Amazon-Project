46958e6c1c55ecec6599676730a18afe89b661b8
ousmane <ousmaneyaou00@gmail.com>
2024-06-28 01:02:32 +0000

    suppression du produit sur la page html

--- script/checkout.js
+++ script/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
         // Générer le HTML pour chaque article dans le panier
         cartSummaryHTML += `
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
                 <div class="delivery-date">
                     Delivery date: Tuesday, June 21
                 </div>
@@ -97,11 +97,15 @@ document.querySelectorAll('.js-delete-link')
 .forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
-        removeFromCart(productId)
-        console.log(cart);
+        removeFromCart(productId);
+        //console.log(cart);
         //updateCartQuantity();
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        console.log(container);
+        container.remove();
     });
 });
+
 
 //pour la mise a jour
 document.querySelectorAll('.js-js-update-link').innerHTML = cartSummaryHTML;
